5539a2175075d8e6fb30b5bba97b36a68e496b89
PRABHAKARAN JAYARAMAN MASANI <prabhakaran.jayaramanmasani@tcs.com>
2019-06-16 21:29:34 +0530

    updated app text

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World from Node App\n');
 });
 
 server.listen(port, hostname, () => {
